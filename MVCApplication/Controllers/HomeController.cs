07640733278be6403a4430a51a114cb90c9b954c
JAYRAJ DATTA <jayrajdatta@gmail.com>
2020-07-05 21:03:10 +0530

    First Commit - asp.net CORE MVC app

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(
+            ILogger<HomeController> logger
+            ,IEmployeeRepository employeeRepository)
+        {
+            _logger = logger;
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id, name = newEmployee.Name });
+            }
+
+            return View();
+        }
+
+        public string Details(int? id, string name)
+        {
+            return "id = " + id.Value.ToString() + " and name = " + name;
+        }
+    }
+}
